c9b28f4121a047089e509e041b738c05842a7e19
Emmanuel Analike <emmanuel@analike.net>
2026-07-29 14:48:48 +0100

    feat: supports level-based logging

--- logger.go
+++ logger.go
@@ -10,12 +10,39 @@ import (
 	"fmt"
 	"os"
 	"runtime"
+	"sync"
 	"time"
 
 	"github.com/rs/zerolog"
 )
 
-var zLog zerolog.Logger
+type Level int
+
+const (
+	LevelInfo Level = iota + 1
+	LevelSuccess
+	LevelWarn
+	LevelError
+	LevelFatal
+)
+
+var (
+	zLog     zerolog.Logger
+	mu       sync.RWMutex
+	curLevel = LevelInfo
+)
+
+func SetLevel(l Level) {
+	mu.Lock()
+	defer mu.Unlock()
+	curLevel = l
+}
+
+func GetLevel() Level {
+	mu.RLock()
+	defer mu.RUnlock()
+	return curLevel
+}
 
 func init() {
 	out := zerolog.ConsoleWriter{TimeFormat: time.RFC3339, Out: os.Stdout}
@@ -51,81 +78,113 @@ func getMessage(val any) string {
 }
 
 func Success(msg any, prefix ...string) {
-	zLog.Info().Msg(addPrefix(getMessage(msg), prefix...))
+	if LevelSuccess >= GetLevel() {
+		zLog.Info().Msg(addPrefix(getMessage(msg), prefix...))
+	}
 }
 
 func Info(msg any, prefix ...string) {
-	zLog.Debug().Msg(addPrefix(getMessage(msg), prefix...))
+	if LevelInfo >= GetLevel() {
+		zLog.Debug().Msg(addPrefix(getMessage(msg), prefix...))
+	}
 }
 
 // Error print ERR msg; adds a trace to the caller
 func Error(msg any, prefix ...string) {
-	zLog.Error().Str("trace", caller()).Msg(addPrefix(getMessage(msg), prefix...))
+	if LevelError >= GetLevel() {
+		zLog.Error().Str("trace", caller()).Msg(addPrefix(getMessage(msg), prefix...))
+	}
 }
 
 // Warn print WRN msg; adds a trace to the caller
 func Warn(msg any, prefix ...string) {
-	zLog.Warn().Str("trace", caller()).Msg(addPrefix(getMessage(msg), prefix...))
+	if LevelWarn >= GetLevel() {
+		zLog.Warn().Str("trace", caller()).Msg(addPrefix(getMessage(msg), prefix...))
+	}
 }
 
 // Fatal print FTL msg; adds a trace to the caller
 func Fatal(msg any, prefix ...string) {
-	zLog.Fatal().Str("trace", caller()).Msg(addPrefix(getMessage(msg), prefix...))
+	if LevelFatal >= GetLevel() {
+		zLog.Fatal().Str("trace", caller()).Msg(addPrefix(getMessage(msg), prefix...))
+	}
 }
 
 // FatalMsg print FTL msg without trace to the caller
 // exits program with code 1
 func FatalMsg(msg any, prefix ...string) {
-	zLog.Fatal().Msg(addPrefix(getMessage(msg), prefix...))
+	if LevelFatal >= GetLevel() {
+		zLog.Fatal().Msg(addPrefix(getMessage(msg), prefix...))
+	}
 }
 
 // ErrorMsg print ERR msg; without trace to the caller
 func ErrorMsg(msg any, prefix ...string) {
-	zLog.Error().Msg(addPrefix(getMessage(msg), prefix...))
+	if LevelError >= GetLevel() {
+		zLog.Error().Msg(addPrefix(getMessage(msg), prefix...))
+	}
 }
 
 // WarnMsg print WRN msg; without trace to the caller
 func WarnMsg(msg any, prefix ...string) {
-	zLog.Warn().Msg(addPrefix(getMessage(msg), prefix...))
+	if LevelWarn >= GetLevel() {
+		zLog.Warn().Msg(addPrefix(getMessage(msg), prefix...))
+	}
 }
 
 // Successf prints a formatted INF message
 func Successf(format string, a ...any) {
-	zLog.Info().Msg(fmt.Sprintf(format, a...))
+	if LevelSuccess >= GetLevel() {
+		zLog.Info().Msg(fmt.Sprintf(format, a...))
+	}
 }
 
 func Infof(format string, a ...any) {
-	zLog.Debug().Msg(fmt.Sprintf(format, a...))
+	if LevelInfo >= GetLevel() {
+		zLog.Debug().Msg(fmt.Sprintf(format, a...))
+	}
 }
 
 // Errorf prints formatted ERR message following defined pattern; adds trace to caller
 func Errorf(format string, a ...any) {
-	zLog.Error().Str("trace", caller()).Msg(fmt.Sprintf(format, a...))
+	if LevelError >= GetLevel() {
+		zLog.Error().Str("trace", caller()).Msg(fmt.Sprintf(format, a...))
+	}
 }
 
 // Fatalf prints formatted FTL message following defined pattern
 // adding trace to caller and exiting with code 1
 func Fatalf(format string, a ...any) {
-	zLog.Fatal().Str("trace", caller()).Msg(fmt.Sprintf(format, a...))
+	if LevelFatal >= GetLevel() {
+		zLog.Fatal().Str("trace", caller()).Msg(fmt.Sprintf(format, a...))
+	}
 }
 
 // Warnf prints formatted ERR message following defined pattern; adds trace to caller
 func Warnf(format string, a ...any) {
-	zLog.Warn().Str("trace", caller()).Msg(fmt.Sprintf(format, a...))
+	if LevelWarn >= GetLevel() {
+		zLog.Warn().Str("trace", caller()).Msg(fmt.Sprintf(format, a...))
+	}
 }
 
 // ErrorMsgf prints formatted ERR message following defined pattern without trace to caller
 func ErrorMsgf(format string, a ...any) {
-	zLog.Error().Msg(fmt.Sprintf(format, a...))
+	if LevelError >= GetLevel() {
+		zLog.Error().Msg(fmt.Sprintf(format, a...))
+	}
 }
 
 // WarnMsgf prints formatted WRN message following defined pattern without trace to caller
 func WarnMsgf(format string, a ...any) {
-	zLog.Warn().Msg(fmt.Sprintf(format, a...))
+	if LevelWarn >= GetLevel() {
+		zLog.Warn().Msg(fmt.Sprintf(format, a...))
+	}
 }
 
 // FatalMsgf prints formatted FTL message following defined pattern without trace to caller
 // exits the program with code 1
 func FatalMsgf(format string, a ...any) {
-	zLog.Fatal().Msg(fmt.Sprintf(format, a...))
+	if LevelFatal >= GetLevel() {
+		zLog.Fatal().Msg(fmt.Sprintf(format, a...))
+	}
 }
